ecf307bde8914090721bac1f29f068a276cc0b67
Dmit Bashmakov <denegniy1@gmail.com>
2024-09-18 08:57:00 +0500

    changes to delivery model

--- distribution/models.py
+++ distribution/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    full_name = models.CharField(max_length=100, verbose_name='имя клиента')
+    full_name = models.CharField(max_length=100, verbose_name='имя получателя')
     email = models.EmailField(verbose_name='электронная почта')
     comment = models.TextField(verbose_name='комментарий')
 
@@ -10,21 +10,8 @@ class Client(models.Model):
         return f'{self.full_name}'
 
     class Meta:
-        verbose_name = 'клиент'
-        verbose_name_plural = 'клиенты'
-
-
-class Delivery(models.Model):
-    is_created = models.DateTimeField(auto_now_add=True, verbose_name='первая отправка')
-    period = models.PositiveIntegerField(verbose_name='периодичность')
-    status = models.CharField(max_length=100, verbose_name='статус рассылки')
-
-    def __str__(self):
-        return f'Отправлено {self.is_created}, повтор каждые {self.period} дн.'
-
-    class Meta:
-        verbose_name = 'рассылка'
-        verbose_name_plural = 'рассылки'
+        verbose_name = 'получатель'
+        verbose_name_plural = 'получатели'
 
 
 class Message(models.Model):
@@ -37,6 +24,34 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'сообщение'
         verbose_name_plural = 'сообщения'
+
+
+class Delivery(models.Model):
+    STATUS_CHOICES = (
+        ('CREATED', 'создана'),
+        ('LAUNCHED', 'запущена'),
+        ('COMPLETED', 'завершена'),
+    )
+    DELIVERY_PERIOD = (
+        ('ONCE', 'однократно'),
+        ('DAILY', 'ежедневно'),
+        ('WEEKLY', 'еженедельно'),
+        ('MONTHLY', 'ежемесячно'),
+    )
+
+    is_created = models.DateTimeField(auto_now_add=True, verbose_name='первая отправка')
+    period = models.CharField(max_length=255, choices=DELIVERY_PERIOD, default='ONCE', verbose_name='периодичность')
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default='CREATED', verbose_name='статус рассылки')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='сообщение', blank=True, null=True)
+    is_active = models.BooleanField(default=True, verbose_name='активная рассылка')
+    email_client = models.ManyToManyField(Client, verbose_name='получатели')
+
+    def __str__(self):
+        return f'Рассылка № {self.pk} от {self.is_created}, периодичность:  {self.period}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
 
 
 class SendTry(models.Model):
