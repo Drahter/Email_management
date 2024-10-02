8dc09d475bcf6775c0e0396e5b9ac23d18a04b85
Dmit Bashmakov <denegniy1@gmail.com>
2024-10-02 13:49:48 +0500

    added CRUD for delivery model

--- distribution/urls.py
+++ distribution/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from distribution.apps import DistributionConfig
-from distribution.views import MessageListView
+from distribution.views import MessageListView, DeliveryListView, DeliveryDetailView, DeliveryUpdateView, \
+    DeliveryDeleteView, DeliveryCreateView
 
 app_name = DistributionConfig.name
 
 urlpatterns = [
-    path('', MessageListView.as_view(), name='message_list')
+    path('', DeliveryListView.as_view(), name='delivery_list'),
+    # path('deliveries/', DeliveryListView.as_view(), name='delivery_list'),
+    path('create/', DeliveryCreateView.as_view(), name='new_product'),
+    path('deliveries/<int:pk>/', DeliveryDetailView.as_view(), name='delivery_detail'),
+    path('deliveries/<int:pk>/update/', DeliveryUpdateView.as_view(), name='delivery_update'),
+    path('deliveries/<int:pk>/delete/', DeliveryDeleteView.as_view(), name='delivery_delete'),
+    path('messages/', MessageListView.as_view(), name='message_list'),
 ]
--- distribution/views.py
+++ distribution/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from distribution.models import Message
+from distribution.models import Message, Delivery
 
 
 class MessageListView(ListView):
     model = Message
 
 
-# Create your views here.
+class DeliveryListView(ListView):
+    model = Delivery
+
+
+class DeliveryDetailView(DetailView):
+    model = Delivery
+
+
+# СДЕЛАТЬ ФОРМУ
+class DeliveryCreateView(CreateView):
+    model = Delivery
+    fields = ['email_client', 'message', 'period', 'message', 'is_active']
+    success_url = reverse_lazy('distribution:delivery_list')
+
+
+class DeliveryUpdateView(UpdateView):
+    model = Delivery
+    fields = ['email_client', 'message', 'period', 'message', 'is_active', 'status']
+    success_url = reverse_lazy('distribution:delivery_list')
+
+
+class DeliveryDeleteView(DeleteView):
+    model = Delivery
+    success_url = reverse_lazy('distribution:delivery_list')
