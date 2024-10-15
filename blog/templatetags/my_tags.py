942a6ff2f87b0d8b1b66924a83ecf4cf1f7484e2
Dmit Bashmakov <denegniy1@gmail.com>
2024-10-15 15:35:49 +0500

    main page shows number of activities, examples of articles in blog

--- blog/templatetags/my_tags.py
+++ blog/templatetags/my_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.filter()
 def media_filter(path):
     if path:
-        return f'/media/blog/article_previews/{path}'
+        return f'/media/{path}'
     return '#'
